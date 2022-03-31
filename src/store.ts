4a04b1a4d6f37bb71d7292613b31ccba0be34dca
Ruhi Bloodworth <ruhi.bloodworth@gmail.com>
2022-03-31 00:34:14 -0500

    feat: persist state to localStorage

--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,7 @@ import { reducer as answer } from "./answer";
 import { reducer as gameState } from "./gameState";
 import { reducer as guesses } from "./guesses";
 import { reducer as letters } from "./letters";
+
 export const config = {
   reducer: {
     answer,
@@ -12,9 +13,16 @@ export const config = {
     guesses,
     letters,
   },
+  preloadedState:
+    JSON.parse(localStorage.getItem("reduxle-state") || "null") || undefined,
 };
 
 export const store = configureStore(config);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+store.subscribe(() => {
+  const state = store.getState();
+  localStorage.setItem("reduxle-state", JSON.stringify(state));
+});
